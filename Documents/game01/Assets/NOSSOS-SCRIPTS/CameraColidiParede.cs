f57835d8aa8c87302e337c9bc10333d26975e314
brunoKoerich <bruno_koerich@hotmail.com>
2017-10-07 13:12:06 -0300

    versao inicial

---
+++ Documents/game01/Assets/NOSSOS-SCRIPTS/CameraColidiParede.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColidiParede : MonoBehaviour {
+
+    public Transform alvo;
+    RaycastHit hit = new RaycastHit();
+    public float mouseX = 0;
+    public float mouseY = 0;
+    public float distCam = 6;
+
+    void Start () {
+	}
+	
+	
+	void Update () {
+        transform.RotateAround(alvo.position, transform.up, Input.GetAxis("Mouse X") * mouseX);
+        transform.RotateAround(alvo.position, transform.right, Input.GetAxis("Mouse Y") * mouseY);
+
+        Vector3 rotacao = transform.eulerAngles;
+        rotacao.z = 0;
+        transform.eulerAngles = rotacao;
+
+        transform.position = alvo.position - transform.forward * distCam;
+
+        if(Physics.Linecast(alvo.position, transform.position, out hit))
+        {
+            transform.position = hit.point + transform.forward * 0.2f;
+        }
+        //se apertar tecla c muda distancia da camera para, 0 aproximando a visao em jogo
+        if (Input.GetKeyDown("c"))
+        {
+            AproximaCamera();
+        }
+    }
+
+    void AproximaCamera()
+    {
+        if (this.distCam == 6)
+        {
+            this.distCam = 0;
+        }
+        else
+        {
+            this.distCam = 6;
+        }
+           
+    }
+}//class
---
+++ Documents/game01/Assets/NOSSOS-SCRIPTS/TeleporteAgua.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporteAgua : MonoBehaviour {
+    /* Este script eu jogo na agua e a variavel destinoTrigger
+       é o objeto que será usado como parametro para teleporta o
+       jogador até o local onde o destinoTrigger estiver  */
+
+
+
+    // variavel que recebe um obj e onde este obj estiver o jogador sera teleportado
+    public Transform destinoTrigger;
+
+	// função que teleporta o jogador se tocar na agua
+	void OnTriggerEnter (Collider col) {
+		if(col.transform.name == "Aluno")
+        {
+            Vector3 newPos = destinoTrigger.position;
+            newPos.y = 7.5f;
+            col.transform.position = newPos;
+            Debug.Log("ola");
+        }
+	}
+}
