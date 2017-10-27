0058a2d08ce7c29619d3b83457fc7afe748be722
brunoKoerich <bruno_koerich@hotmail.com>
2017-10-26 22:22:06 -0200

    Adicionado imagens no painel dos botoes

--- Documents/game01/Assets/PaineisSevProtInterf.cs
+++ Documents/game01/Assets/PaineisSevProtInterf.cs
@@ -7,15 +7,18 @@ public class PaineisSevProtInterf : MonoBehaviour {
     //criando um obj para poder intancia-lo depois podendo asism exibir no game com SetActive(true)
     [SerializeField] GameObject painelServicos;
     [SerializeField] GameObject painelProtocolos;
+    [SerializeField] GameObject painelInterfaces;
 
     // Inicia escondendo os paineis
     public void Start () {
         //astribuindo o obj com a tag PainelServico no obj "painelServicos"
         painelServicos = GameObject.FindGameObjectWithTag("PainelServico");
         painelProtocolos = GameObject.FindGameObjectWithTag("PainelProtocolo");
+        painelInterfaces = GameObject.FindGameObjectWithTag("PainelInterface");
         //escondendo o painel
         painelServicos.SetActive(false);
         painelProtocolos.SetActive(false);
+        painelInterfaces.SetActive(false);
     }
 	
 	// Exibe ou esconde o painel Serviços ao clicar no botão serviços
@@ -43,6 +46,21 @@ public class PaineisSevProtInterf : MonoBehaviour {
         else
         {
             painelProtocolos.SetActive(true);
+        }
+
+    }
+
+    // Exibe ou esconde o painel Interface ao clicar no botão interface
+    public void ExibeEscondePainelIntreface()
+    {
+        //activeSelf retorna o valor boleano do obj - se o painel estiver aberto esconde
+        if (painelInterfaces.activeSelf)
+        {
+            painelInterfaces.SetActive(false);
+        } // se não exibe o painel
+        else
+        {
+            painelInterfaces.SetActive(true);
         }
 
     }
