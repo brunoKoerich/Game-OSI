7c4bdfb228369b49d27d14b1a0c928d21cfbb326
Gustavo Gregório <gustgreg94@gmail.com>
2017-10-28 22:57:41 -0200

    Adicionado colisao encima do carrinho Não deixa sair quando carrinho estiver andando Arrumado botao continuar Esticada ponte e levantada

--- Documents/game01/Assets/NOSSOS-SCRIPTS/Veiculo.cs
+++ Documents/game01/Assets/NOSSOS-SCRIPTS/Veiculo.cs
@@ -14,6 +14,7 @@ public class Veiculo : MonoBehaviour {
 	private bool jogadorDentro = false;
 	private bool jogadorPerto = false;
 
+	private Rigidbody rigidbody;
 	private SimpleCarController carroControle;
 	private GameObject jogador;
 
@@ -64,30 +65,55 @@ public class Veiculo : MonoBehaviour {
 	}
 
 	private void Entrar () {
-		if (this.jogadorPerto && !this.jogadorDentro) {
-			this.jogadorDentro = true;
-
-			this.cameraVeiculo.gameObject.SetActive (true);
-
-			this.carroControle.enabled = true;
-
-			this.jogador.SetActive (false);
-			this.jogador.transform.SetParent (this.transform);
+		// Cancela caso jogador esteja dentro ou longe do veiculo
+		if (!this.jogadorPerto || this.jogadorDentro) {
+			return;
 		}
+
+		this.jogadorDentro = true;
+
+		// Ativa camera do jogador
+		this.cameraVeiculo.gameObject.SetActive (true);
+
+		// Ativa comandos do veiculo e fisica
+		this.carroControle.enabled = true;
+
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+		rb.isKinematic = false;
+
+		// Desativa jogador e coloca "dentro" do veiculo
+		this.jogador.SetActive (false);
+		this.jogador.transform.SetParent (this.transform);
 	}
 
 	private void Sair () {
-		if (this.jogadorDentro) {
-			this.jogadorDentro = false;
-
-			this.carroControle.enabled = false;
-			this.carroControle.transform.rotation = Quaternion.Euler(0, this.carroControle.transform.eulerAngles.y, 0);
-
-			this.jogador.transform.SetParent (null);
-			this.jogador.transform.rotation = Quaternion.Euler(0, this.jogador.transform.eulerAngles.y, 0);
-			this.jogador.SetActive (true);
-
-			this.cameraVeiculo.gameObject.SetActive (false);
+		// Cancela caso jogador esteja fora do veiculo
+		if (!this.jogadorDentro) {
+			return;
 		}
+
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+
+		// Cancela caso esteja andando rápido
+		if (rb.velocity.magnitude > 1) {
+			return;
+		}
+
+		this.jogadorDentro = false;
+
+		// Desativa comando do veiculo e arruma sua posição
+		this.carroControle.enabled = false;
+		this.carroControle.transform.rotation = Quaternion.Euler(0, this.carroControle.transform.eulerAngles.y, 0);
+
+		// Para o veiculo
+		rb.isKinematic = true;
+
+		// Ativa jogador, arruma sua posição e tira de "dentro" do veiculo
+		this.jogador.transform.SetParent (null);
+		this.jogador.transform.rotation = Quaternion.Euler(0, this.jogador.transform.eulerAngles.y, 0);
+		this.jogador.SetActive (true);
+
+		// Desabilita camera do veiculo
+		this.cameraVeiculo.gameObject.SetActive (false);
 	}
 }
