93c1b2beea88e77993536fd934283cbe42e478d2
brunoKoerich <bruno_koerich@hotmail.com>
2017-11-04 01:01:32 -0200

    axei que tinha acabado, pura ilusao3

---
+++ Documents/game01/Assets/NOSSOS-SCRIPTS/BtnIniciarGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnIniciarGame : MonoBehaviour {
+
+    [SerializeField] GameObject painelInicial;
+
+    public void IniciarGame()
+    {
+        painelInicial = GameObject.FindGameObjectWithTag("PainelInicial");
+        painelInicial.SetActive(false);
+    }
+}
